adfae94c24bc36f34a4b12e6f38f242194002317
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-02 21:30:46 +0200

    update province repos

--- src/main/java/com/epic_energies/business/repository/ProvinceRepository.java
+++ src/main/java/com/epic_energies/business/repository/ProvinceRepository.java
@@ -10,7 +10,8 @@ import com.epic_energies.business.model.Province;
 @Repository
 public interface ProvinceRepository extends CrudRepository<Province, Long> {
 	
+	Province findByName(String name);
+	Province findByAbbr(String abbr);
 	List<Province> findByCounty(String county);
-	List<Province> findByName(String name);
 	
 }
