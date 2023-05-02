325bbe0f93ad7adea844c7faf738c872aac0c2f7
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-02 19:33:19 +0200

    update municipality class / entity

--- src/main/java/com/epic_energies/business/model/Municipality.java
+++ src/main/java/com/epic_energies/business/model/Municipality.java
@@ -3,11 +3,19 @@ package com.epic_energies.business.model;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.epic_energies.business.service.MunicipalityService;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,35 +31,18 @@ import lombok.Setter;
 @Setter
 public class Municipality {
 	
+	@Transient 
+	@Autowired private static MunicipalityService muniService;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private Integer historical_code;
-	private Integer progressive_code;
+	
+	@Column(nullable = false)
 	private String name;
-	private String province;
 	
-	public static void importMunicipalities() throws FileNotFoundException {
-		Scanner sc = new Scanner(new File("src\\main\\resources\\data\\comuni-italiani.csv"));
-		sc.useDelimiter(",");
-		
-		while (sc.hasNext()) {
+	@ManyToOne
+	private Province province;
 	
-			String[] fileToString = sc.next().split("\n");
-			
-			for (int i = 1; i < fileToString.length; i++) {
-				String[] mToString = fileToString[i].split(";");
-				Municipality m = Municipality.builder()
-						.historical_code(Integer.parseInt(mToString[0]))
-						.progressive_code(Integer.parseInt(mToString[1]))
-						.name(mToString[2]).province(mToString[3]).build();
-				
-				System.out.println(m);
-				//QUI METODO SAVE DEL SERVICE PER SALVARE SU DB
-				
-			}
-		}
 		
-		sc.close();
-	}
 }
