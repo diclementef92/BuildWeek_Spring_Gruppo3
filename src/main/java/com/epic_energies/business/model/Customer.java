7134b5b4762e1ad91ef5c9460d7c31c8e2019aff
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-02 22:38:19 +0200

    adds builder to customer class / entity

--- src/main/java/com/epic_energies/business/model/Customer.java
+++ src/main/java/com/epic_energies/business/model/Customer.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -18,6 +19,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "customers")
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Data
 public class Customer {
     @Id
