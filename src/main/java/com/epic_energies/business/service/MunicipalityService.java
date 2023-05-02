e1133861a03a73a75fef5820715291aaa78d8126
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-02 19:34:52 +0200

    adds municipality service

---
+++ src/main/java/com/epic_energies/business/service/MunicipalityService.java
@@ -0,0 +1,60 @@
+package com.epic_energies.business.service;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.epic_energies.business.model.Municipality;
+import com.epic_energies.business.model.Province;
+import com.epic_energies.business.repository.MunicipalityRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class MunicipalityService {
+
+	@Autowired MunicipalityRepository muniRepo;
+	@Autowired ProvinceService provService;
+	
+	// internal methods
+	
+	public void importAllMunicipalities() throws FileNotFoundException {
+		Scanner sc = new Scanner(new File("src\\main\\resources\\data\\comuni-italiani.csv"));
+		sc.useDelimiter(",");
+		
+		while (sc.hasNext()) {
+	
+			String[] fileToString = sc.next().split("\n");
+			
+			List<Province> allProvince =  provService.findAllProvinces();
+			
+			for (int i = 1; i < fileToString.length; i++) {
+				String[] mToString = fileToString[i].split(";");
+				
+				List<Province> relatedProvince = allProvince.stream()
+						.filter(p -> p.getName().toLowerCase().replaceAll("\\s", "").equalsIgnoreCase(mToString[3].toLowerCase().replaceAll("\\s", "")))
+						.collect(Collectors.toList());
+				if (relatedProvince.size() < 1) {
+					log.warn(mToString[3]);
+				}
+				persistMunicipality(Municipality.builder().name(mToString[2]).province(relatedProvince.get(0)).build());				
+			}
+		}
+		sc.close();
+	}	
+	
+	// jpa methods
+	public void persistMunicipality(Municipality m) {
+		muniRepo.save(m);
+		log.info("municipality correctly persited on Database");
+	}
+	
+	
+	
+}
