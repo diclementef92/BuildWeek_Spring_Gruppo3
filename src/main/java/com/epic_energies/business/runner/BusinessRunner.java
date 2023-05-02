88642dcc135665533d0a01fd0b1204c712da4166
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-02 22:38:47 +0200

    test in runner

--- src/main/java/com/epic_energies/business/runner/BusinessRunner.java
+++ src/main/java/com/epic_energies/business/runner/BusinessRunner.java
@@ -10,15 +10,13 @@ import com.epic_energies.business.service.CustomerService;
 @Component
 public class BusinessRunner implements ApplicationRunner {
 
-    @Autowired
-    CustomerService custom;
-
+    @Autowired CustomerService custService;
     @Override
     public void run(ApplicationArguments args) throws Exception {
 	System.out.println("Customer creato!");
-	custom.createopAdress();
-	custom.createoperAdress();
-	custom.createCustomer();
+	
+	custService.persistFakeCustomer();
+	
     }
 
 }
