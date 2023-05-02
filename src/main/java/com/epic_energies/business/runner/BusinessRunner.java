f4b3b1100189fbf7e54b38fc2df661c9266dcc9c
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-02 19:42:41 +0200

    create conditionals to popoulate db in case of no objs

--- src/main/java/com/epic_energies/business/runner/BusinessRunner.java
+++ src/main/java/com/epic_energies/business/runner/BusinessRunner.java
@@ -1,16 +1,33 @@
 package com.epic_energies.business.runner;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import com.epic_energies.business.service.MunicipalityService;
+import com.epic_energies.business.service.ProvinceService;
+
 @Component
 public class BusinessRunner implements ApplicationRunner {
 
+	@Autowired ProvinceService proService;
+	@Autowired MunicipalityService munService;
+	
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		// TODO Auto-generated method stub
-
+		
+		// IF TABLE PROVINCES ON DB HAS LESS THAN 1 ROW IN IT, METHOD WILL READ THE .CLS FILE AND POPULATE THE DB
+		if(proService.findAllProvinces().size() < 1) {
+			proService.importProvinces();
+		}
+		
+		// IF TABLE MUNICIPALITIES ON DB HAS LESS THAN 1 ROW IN IT, METHOD WILL READ THE .CLS FILE AND POPULATE THE DB
+		if (munService.findAllMunicipality().size() < 1) {
+			munService.importAllMunicipalities();
+		}
+		
 	}
 
 }
