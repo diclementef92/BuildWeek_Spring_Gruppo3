a2ee04fe03f6639767ddf731170586fe7e12d121
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-03 15:02:18 +0200

    update of address classes

--- src/main/java/com/epic_energies/business/model/Address.java
+++ src/main/java/com/epic_energies/business/model/Address.java
@@ -6,24 +6,28 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "legal_adresses")
+@Table(name = "operative_adresses")
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Data
-public class LegalAdress {
+public class Address {
+	
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String streetName;
     private Integer streetNumber;
     private String place;
-    private Integer postCode;
+    private Integer postCode;	
+    private E_AddressType addressType;
 
-    // @ManyToOne
-    // private String Municipality;
+//  @ManyToOne
+//  private String Municipality;
 
 }
---
+++ src/main/java/com/epic_energies/business/model/E_AddressType.java
@@ -0,0 +1,8 @@
+package com.epic_energies.business.model;
+
+public enum E_AddressType {
+	
+	LEGAL_ADDRESS,
+	OPERATIVE_ADDRESS
+	
+}
