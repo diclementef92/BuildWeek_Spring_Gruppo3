ac358d1b7029461d0025cd283074d9196f09d8af
Giacomo Della Peruta <giax.95@live.it>
2023-05-04 12:23:47 +0200

    Fixes

--- src/main/java/com/epic_energies/business/controller/CustomerController.java
+++ src/main/java/com/epic_energies/business/controller/CustomerController.java
@@ -20,50 +20,43 @@ import com.epic_energies.business.service.CustomerService;
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
-	
-	@Autowired CustomerService cusService;
+
+	@Autowired
+	CustomerService cusService;
 
 	@GetMapping("/{id}")
 	public ResponseEntity<?> findCustomerById(@PathVariable Long id) {
 		return new ResponseEntity<Customer>(cusService.findCustomerById(id), HttpStatus.FOUND);
 	}
-	
+
 	@GetMapping
 	public ResponseEntity<?> findAllCustomer() {
 		return new ResponseEntity<List<Customer>>(cusService.findAll(), HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/paged")
 	public ResponseEntity<?> findAllCustomersPaged(Pageable pageable) {
 		return new ResponseEntity<Page<Customer>>(cusService.findAll(pageable), HttpStatus.FOUND);
 	}
-	
-	@GetMapping("/paged/{name}")
-	public ResponseEntity<?> findAllCustomersPagedByBusinessName(@PathVariable String businessName, Pageable pageable) {
-		return new ResponseEntity<Page<Customer>>(cusService.findAllByBusinessName(businessName, pageable), HttpStatus.FOUND);
-	}
-	
-	// QUI I VARI GETTERS COMPLESSI E PAGEABLE
-	
+
 	@PostMapping
 	public ResponseEntity<?> addNewCustomer(@RequestBody Customer c) {
 		return new ResponseEntity<String>(cusService.persistCustomer(c), HttpStatus.CREATED);
 	}
-	
+
 	@PutMapping("/{id}")
 	public ResponseEntity<?> updateCustomer(@RequestBody Customer c) {
 		return new ResponseEntity<String>(cusService.updateCustomer(c), HttpStatus.OK);
 	}
-	
+
 	@DeleteMapping
 	public ResponseEntity<?> deleteCustomer(@RequestBody Customer c) {
 		return new ResponseEntity<String>(cusService.deleteCustomer(c), HttpStatus.OK);
 	}
-	
+
 	@DeleteMapping("{id}")
 	public ResponseEntity<?> deleteCustomer(@PathVariable Long id) {
 		return new ResponseEntity<String>(cusService.deleteCustomer(id), HttpStatus.OK);
 	}
-	
-	
+
 }
