6c8cd3ca8d6ce1e8866e7b0fc03dd90b2d208fcf
Luca Scomazzon <scom.luca@gmail.com>
2023-05-04 12:30:21 +0200

    Add pagination Invoice

--- src/main/java/com/epic_energies/business/repository/InvoiceDao.java
+++ src/main/java/com/epic_energies/business/repository/InvoiceDao.java
@@ -5,8 +5,11 @@ import java.sql.Date;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.epic_energies.business.model.Customer;
@@ -14,34 +17,32 @@ import com.epic_energies.business.model.Invoice;
 import com.epic_energies.business.model.InvoiceStatus;
 
 @Repository
-public interface InvoiceDao extends JpaRepository<Invoice, Long> {
+public interface InvoiceDao extends JpaRepository<Invoice, Long>, PagingAndSortingRepository<Invoice, Long> {
 
     Optional<Invoice> findById(Long id);
 
     // Filter per cliente
     List<Invoice> findByCustomer(Customer customer);
-    
+
     // filter per stato
     List<Invoice> findByInvoiceStatus(InvoiceStatus invoiceStatus);
-    
+
     // filter per data precisa
     List<Invoice> findByDate(Date date);
-    
+
     // filter date in range
-    
-    
+
     // filter per anno
     List<Invoice> findByYear(Integer year);
-    
+
     // filter importo fino a...
     List<Invoice> findByAmountLessThan(BigDecimal amount);
-    
+
     // filter importo da...
     List<Invoice> findByAmountGreaterThan(BigDecimal amount);
-    
+
     // filter per range d'importi
-    
-    
+
     // ordine crescente degli importi fattura
     @Query("SELECT i FROM Invoice i ORDER BY i.amount ASC")
     Optional<List<Invoice>> findByAmount();
@@ -54,4 +55,6 @@ public interface InvoiceDao extends JpaRepository<Invoice, Long> {
     @Query("SELECT i FROM Invoice i ORDER BY i.year DESC")
     Optional<List<Invoice>> findByYear();
 
+    // PAGEABLE QUERIES
+    Page<Invoice> findAll(Pageable pageable);
 }
--- src/main/java/com/epic_energies/business/service/InvoiceService.java
+++ src/main/java/com/epic_energies/business/service/InvoiceService.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.epic_energies.business.model.Customer;
@@ -19,7 +21,7 @@ import jakarta.persistence.EntityNotFoundException;
 @Service
 public class InvoiceService {
     @Autowired
-    private InvoiceDao fatturaDao;
+    private InvoiceDao invoiceDao;
 
     @Autowired
     private CustomerService customerService;
@@ -27,72 +29,75 @@ public class InvoiceService {
     @Autowired
     @Qualifier("FakeInvoice")
     private ObjectProvider<Invoice> objFattura;
-    
+
     private Faker fake = Faker.instance(new Locale("en-EN"));
 
     public void persistInvoice(Invoice i, Long id) {
-		Customer c = customerService.findCustomerById(id);
-		i.setCustomer(c);
-		
-		fatturaDao.save(i);
+	Customer c = customerService.findCustomerById(id);
+	i.setCustomer(c);
+
+	invoiceDao.save(i);
     }
 
     public void createInvoice(Long id) {
-    	persistInvoice(objFattura.getObject(), id);
+	persistInvoice(objFattura.getObject(), id);
     }
 
     public void createFakeInvoice() {
-		
-		List<Customer> list_customer = customerService.findAll();
-		Integer random = fake.number().numberBetween(0, list_customer.size() - 1);
-		Customer c = list_customer.get(random);
-		Invoice i = objFattura.getObject();
-		
-		i.setCustomer(c);
-		fatturaDao.save(i);
+
+	List<Customer> list_customer = customerService.findAll();
+	Integer random = fake.number().numberBetween(0, list_customer.size() - 1);
+	Customer c = list_customer.get(random);
+	Invoice i = objFattura.getObject();
+
+	i.setCustomer(c);
+	invoiceDao.save(i);
     }
 
     public String updateInvoice(Invoice i) {
-    	if (fatturaDao.existsById(i.getId())) {
-    		fatturaDao.save(i);
-    		return "Invoice correctly updated on Database";
-    	} else {
-    		throw new EntityNotFoundException("Invoice with ID --> " + i.getId() + " doesn't exists on Database!");
-    	}
+	if (invoiceDao.existsById(i.getId())) {
+	    invoiceDao.save(i);
+	    return "Invoice correctly updated on Database";
+	} else {
+	    throw new EntityNotFoundException("Invoice with ID --> " + i.getId() + " doesn't exists on Database!");
+	}
     }
-    
+
     public String deleteInvoice(Invoice i) {
-    	if (fatturaDao.existsById(i.getId())) {
-    		fatturaDao.delete(i);
-    		return "Invoice correctly deleted from Database";
-    	} else {
-    		throw new EntityNotFoundException("Invoice with ID --> " + i.getId() + " doesn't exists on Database!");
-    	}
+	if (invoiceDao.existsById(i.getId())) {
+	    invoiceDao.delete(i);
+	    return "Invoice correctly deleted from Database";
+	} else {
+	    throw new EntityNotFoundException("Invoice with ID --> " + i.getId() + " doesn't exists on Database!");
+	}
     }
-    
+
     public String deleteInvoice(Long id) {
-    	if (fatturaDao.existsById(id)) {
-    		fatturaDao.deleteById(id);
-    		return "Invoice correctly deleted from Database";
-    	} else {
-    		throw new EntityNotFoundException("Invoice with ID --> " + id + " doesn't exists on Database!");
-    	}
+	if (invoiceDao.existsById(id)) {
+	    invoiceDao.deleteById(id);
+	    return "Invoice correctly deleted from Database";
+	} else {
+	    throw new EntityNotFoundException("Invoice with ID --> " + id + " doesn't exists on Database!");
+	}
     }
 
     public Invoice FindInvoiceById(Long id) {
-		if (fatturaDao.existsById(id)) {
-		    return fatturaDao.findById(id).get();
-		} else {
-		    throw new EntityNotFoundException("Invoice with ID --> " + id + " doesn't exists on Database!");
-		}
+	if (invoiceDao.existsById(id)) {
+	    return invoiceDao.findById(id).get();
+	} else {
+	    throw new EntityNotFoundException("Invoice with ID --> " + id + " doesn't exists on Database!");
+	}
     }
 
     public Optional<List<Invoice>> getAllInvoicesOrderByLastData() {
-    	return fatturaDao.findByDate();
+	return invoiceDao.findByDate();
     }
 
     public List<Invoice> findAll() {
-    	return (List<Invoice>) fatturaDao.findAll();
+	return (List<Invoice>) invoiceDao.findAll();
     }
 
+    public Page<Invoice> findAll(Pageable pageable) {
+	return (Page<Invoice>) invoiceDao.findAll(pageable);
+    }
 }
