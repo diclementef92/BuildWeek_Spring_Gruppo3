d0a1251eabf1b900656cf02b3bd0f3eeea84dfd9
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-05 13:18:55 +0200

    adds Paged req by address type

--- src/main/java/com/epic_energies/business/repository/AddressDAO.java
+++ src/main/java/com/epic_energies/business/repository/AddressDAO.java
@@ -7,9 +7,11 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import com.epic_energies.business.model.Address;
 import com.epic_energies.business.model.Customer;
+import com.epic_energies.business.model.E_AddressType;
 
 @Repository
 public interface AddressDAO extends CrudRepository<Address, Long>, PagingAndSortingRepository<Address, Long> {
 	// PAGEABLE QUERIES
 	Page<Address> findAll(Pageable pageable);
+	Page<Address> findByAddressType(E_AddressType addressType, Pageable pageable);
 }
